1e5bf109d3470ce9785e7b148611732fba242484
Miguel Ulloa <prueba@correo.com>
2024-03-07 12:59:53 -0600

    MVC architecture implemented
    The project was organized according to the Model View Controller architecture

--- MySocialMediaApp/Controller/LoginViewController.swift
+++ MySocialMediaApp/Controller/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  MySocialMediaApp
 //
 //  Created by Miguel Ulloa on 07/03/24.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.title = "Log In"
+        
     }
 
-
 }
-
